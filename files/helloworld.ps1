46c5450b18d571e536a72ec20e197ebdfb044114
panchakarlabk <panchakarlabk@gmail.com>
2022-07-26 17:36:24 +0100

    aade

--- files/helloworld.ps1
+++ files/helloworld.ps1
@@ -1,5 +1,10 @@
-# Filename: helloworld.ps1
-Write-Host
-Write-Host 'Hello World!'
-Write-Host "Good-this power shell script! "
-# end of script
+#Remove-ADObject -Identity "OU=aduser,OU=UserAccounts,DC=mylab,DC=com" -Recursive 
+#Remove-ADObject -Identity 'CN=AdComputer,CN=Computers,DC=mylab,DC=com' -Recursive
+#Remove-ADObject -Identity "8c6dae68-6cfe-4075-8599-9f461a50d7ec" -Confirm:$False
+
+$name = $env:computername
+
+$GuidID = Get-ADComputer -Identity "$name" -Properties *
+$name
+ 
+#Remove-ADObject -Identity "$GuidID" -Confirm:$False 
