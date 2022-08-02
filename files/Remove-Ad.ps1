211a77fb087d1ae6f5130090b8331202937aedd8
panchakarlabk <panchakarlabk@gmail.com>
2022-08-02 19:31:30 +0100

    ssss

--- files/Remove-Ad.ps1
+++ files/Remove-Ad.ps1
@@ -1,2 +1,6 @@
-$Instance_name
+param 
+( 
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$Instance_name
+)
 Get-ADComputer -Identity "$Instance_name" | Remove-ADObject -Recursive -Confirm:$false
